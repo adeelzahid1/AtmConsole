4d3be8fc710f231fad4e74579b1e391353e62dca
AdeelZahid1 <developer.adeelzahid@gmail.com>
2023-02-03 23:05:41 +0500

    init

---
+++ AtmConsole/AtmConsole.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AtmConsole
+{
+    internal class AtmConsole
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Title = "SUPER ATM";
+
+
+            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("\n\n-----------------Welcome to My ATM App-----------------\n\n");
+
+            Console.WriteLine("Please insert your ATM card");
+            Console.WriteLine("Note: Actual ATM machine will accept and validate" +
+                 "a physical ATM card, read the card number and validate it.");
+
+            Console.WriteLine("\n\nPress Enter to continue...\n");
+            Console.ReadLine();
+
+        }
+    }
+}
